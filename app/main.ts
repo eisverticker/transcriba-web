2fd8b3173a4bcddc5d345b7eaaad6e500d234074
eisverticker <eisverticker@gmail.com>
2016-11-03 14:08:19 +0100

    back to jit compiling

--- app/main.ts
+++ app/main.ts
@@ -2,11 +2,11 @@ import {enableProdMode} from '@angular/core';
 enableProdMode();
 
 //just-in-time option (development)
-//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-//import { AppModule } from './app.module';
-//const platform = platformBrowserDynamic();
-//platform.bootstrapModule(AppModule);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
 //ahead-of-time option (production)
-import { platformBrowser }    from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+//import { platformBrowser }    from '@angular/platform-browser';
+//import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+//platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
