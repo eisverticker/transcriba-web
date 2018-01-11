1293c535c14962dfe2f5ec271b3a52f301fae383
eisverticker <eisverticker@posteo.de>
2018-01-11 03:06:39 +0100

    UtilityModule: extended backend service with urlBuilder

--- src/app/utility/backend.service.ts
+++ src/app/utility/backend.service.ts
@@ -1,33 +1,57 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs/Rx';
 import { environment } from '../../environments/environment';
+import  urlFactory  from 'url-factory';
 
 @Injectable()
 export class BackendService {
   // FIXME remove error observable and methods if there is no use case
   public error: Observable<string>;
   private errorSubject: Subject<string> = new Subject<string>();
+  private urlBuilder: any;
 
   constructor() {
     this.error = this.errorSubject.asObservable();
+    // NOTE: urlBuilder was introduced with Transcriba2
+    //  However, for compatibility reasons the old BackendService api was kept
+    //  and thus the code became uglier
+    let baseUrl = environment.backendApiUrl;
+    if (baseUrl.charAt(baseUrl.length - 1) == '/') {
+      baseUrl = baseUrl.substr(0, baseUrl.length - 1);
+    }
+    this.urlBuilder = urlFactory(baseUrl);
   }
 
-  authUrl(ressourceUri: string, token: string, filter?: string) {
+  authUrl(ressourceUriPart: string, token: string, filter?: string | {[key: string]: string} ) {
+    if (typeof filter === 'object') {
+      return this.urlBuilder(
+        ressourceUriPart,
+        Object.assign({access_token: token}, filter)
+      );
+    }
+
     if (filter === undefined) {
       filter = '';
     }else {
       filter = '&' + filter;
     }
-    return environment.backendApiUrl + ressourceUri + '?access_token=' + token + filter;
+    return environment.backendApiUrl + ressourceUriPart + '?access_token=' + token + filter;
   }
 
-  unAuthUrl(ressourceUri: string, filter?: string): string {
+  unAuthUrl(ressourceUriPart: string, filter?: string | {[key: string]: string}): string {
+    if (typeof filter === 'object') {
+      return this.urlBuilder(
+        ressourceUriPart,
+        filter
+      );
+    }
+
     if (filter === undefined) {
       filter = '';
     }else {
       filter = '?' + filter;
     }
-    return environment.backendApiUrl + ressourceUri + filter;
+    return environment.backendApiUrl + ressourceUriPart + filter;
   }
 
   /**
